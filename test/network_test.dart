407a0cb72ee34796c8509c60de32969122a59295
JShamar <jameelshammr@gmail.com>
2020-07-14 15:58:38 +0800

    add NetWork Test

---
+++ test/network_test.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:open_weather_map_flutter/home/models/result.dart';
+import 'package:open_weather_map_flutter/home/models/weather.dart';
+import 'package:open_weather_map_flutter/home/network/network.dart';
+import 'package:open_weather_map_flutter/home/network/utils/request_type.dart';
+import 'package:open_weather_map_flutter/utils/constants.dart';
+
+void main () {
+  Network networkClient = Network(Client());
+  Result result;
+  group('Network Test', () {
+
+    test("Get Weather Test", () async {
+      try {
+
+        final response = await networkClient.request(requestType: RequestType.GET,
+            path: "/data/2.5/weather?q=London&appid=${Constants().getApiKey()}");
+        if (response.statusCode == 200) {
+          result =  Result<WeatherAPI>.sucesss(WeatherAPI.fromJson(jsonDecode(response.body)));
+        } else {
+          result =  Result.error("Weather not available");
+        }
+
+      } catch (error){
+        result =  Result.error("Something went wrong!");
+
+      }
+
+      WeatherAPI weatherAPI = await ((result as SuccessState).msg as WeatherAPI);
+      expect(weatherAPI.id, 2643743);
+      expect(weatherAPI.name, "London");
+      expect(weatherAPI.cod, 200);
+
+
+    });
+
+
+
+
+
+  });
+}
