8eb67b07cd86b754738c5868d8fb1640d90f40f6
JShamar <jameelshammr@gmail.com>
2020-07-14 15:26:51 +0800

    create  Constants class for network utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+ class Constants {
+  String  _baseUrl = "https://samples.openweathermap.org";
+  String _apiKey = "1da6712886d3a48b9ff4fe48895916c6";
+
+
+  String getBasedUrl () {
+    return this._baseUrl;
+  }
+
+  String getApiKey () {
+    return this._apiKey;
+  }
+
+}
